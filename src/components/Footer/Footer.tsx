46eff0c16707042aa4cba66ffc1edaedb418f73d
Alejandro Suero <asuerome@gmail.com>
2024-01-30 17:02:20 +0100

    feat: First render and breakdown in components

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from "@components/Footer/Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className={styles.footerText}>Made by <strong>Alejandro Suero Mejías</strong></p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/GameBoard/GameBoard.tsx
@@ -0,0 +1,13 @@
+import styles from "@components/GameBoard/GameBoard.module.css";
+
+const cards: number[] = new Array(12).fill(0);
+
+const GameBoard = () => {
+  return (
+    <main className={styles.board}>
+      {cards.map((card, index) => (<div key={index} className={styles.card}>{card}</div>))}
+    </main>
+  );
+};
+
+export default GameBoard;
