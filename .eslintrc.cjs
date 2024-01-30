77450b7537f49604c2f7e01c687aa503689d53ea
Alejandro Suero <asuerome@gmail.com>
2024-01-30 18:38:12 +0100

    feat: Added prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,46 +1,32 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
     "standard-with-typescript",
-    "plugin:react/recommended"
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
   ],
   overrides: [
     {
       env: {
-        node: true
+        node: true,
       },
-      files: [
-        ".eslintrc.{js,cjs}"
-      ],
+      files: [".eslintrc.{js,cjs,json}", ".prettierrc.{js,cjs,json}"],
       parserOptions: {
-        sourceType: "script"
-      }
-    }
+        sourceType: "script",
+      },
+    },
   ],
   parserOptions: {
     ecmaVersion: "latest",
-    project: "./tsconfig.json"
+    project: "./tsconfig.json",
   },
-  plugins: [
-    "react"
-  ],
+  plugins: ["react", "@typescript-eslint", "prettier"],
   rules: {
-    semi: "off",
-    "@typescript-eslint/semi": ["error", "always"],
-    "@typescript-eslint/quotes": [
-      "error",
-      "double"
-    ],
     "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-floating-promises": "off",
-    "jsx-quotes": [
-      "error",
-      "prefer-double"
-    ],
-    "react/jsx-indent": "off",
-    "react/react-in-jsx-scope": "off"
-  }
+    "react/react-in-jsx-scope": "off",
+  },
 };
